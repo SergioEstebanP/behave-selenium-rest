d7214fe3008b5f10fa8135645ff760c8e96e0978
Sergio Esteban <Sergio.Esteban@externas.adidas.com>
2019-09-18 16:00:18 +0200

    Making some improvements in the code

--- features/steps/stepDefinitions.py
+++ features/steps/stepDefinitions.py
@@ -1,8 +1,5 @@
 from behave import given, when, then
 from utils import *
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-import time
 
 
 @given(u'I get information from this API "{api_url}"')
@@ -13,18 +10,12 @@ def get_info_from_api(context, api_url):
 
 @when(u'then I search each pet in "{search_engine}"')
 def search_results_in_browser(context, search_engine):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument("--test-type")
-    driver = webdriver.Chrome(executable_path="C:\\Users\\sergio.esteban\\Downloads\\chromedriver_win32\\chromedriver.exe", port=9515, chrome_options=options)
+    driver = set_browser()
+    if search_engine == "google":
+        search_engine_url = "http://www.google.come"
     for pet_name in context.pet_names:
-        driver.get('https://www.google.com')
-        elem = driver.find_element_by_name("q")
-        time.sleep(3)
-        elem.send_keys(pet_name)
-        elem.send_keys(Keys.RETURN)
-        time.sleep(3)
-
+        search_element(pet_name, driver, search_engine_url)
+        
 
 @then(u'I see the first result in the results page')
 def open_results_page(context):
--- features/steps/utils.py
+++ features/steps/utils.py
@@ -1,5 +1,8 @@
 import json
 from typing import List
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
 import requests
 
@@ -13,3 +16,18 @@ def get_info_api(api_url):
             names.append(group[key]['name'])
 
     return names
+
+def set_browser():
+    options = webdriver.ChromeOptions()
+    options.add_argument('--ignore-certificate-errors')
+    options.add_argument("--test-type")
+    driver = webdriver.Chrome(executable_path="chrome_driver\\chromedriver.exe", port=9515, chrome_options=options)
+    return driver
+
+def search_element(pet_name, driver, search_engine_url):
+    driver.get(search_engine_url)
+    elem = driver.find_element_by_name("q")
+    time.sleep(3)
+    elem.send_keys(pet_name)
+    elem.send_keys(Keys.RETURN)
+    time.sleep(3)
